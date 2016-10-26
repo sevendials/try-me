e5aec7f0d06ca59a6248ae362dbfa6efd3afd930
Christopher Jenkins <christj@gmail.com>
2016-10-26 12:49:15 -0700

    update to alpha 11

--- powershell/puppet/environments/production/manifests/site.pp
+++ powershell/puppet/environments/production/manifests/site.pp
@@ -1,7 +1,7 @@
-package { ['libicu', 'libunwind']: }
+package { ['libicu', 'libunwind','uuid']: }
 ->
 package { 'powershell':
-  source   => 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.9/powershell-6.0.0_alpha.9-1.el7.centos.x86_64.rpm',
+  source   => 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.11/powershell-6.0.0_alpha.11-1.el7.centos.x86_64.rpm',
   provider => 'rpm',
 }
 
